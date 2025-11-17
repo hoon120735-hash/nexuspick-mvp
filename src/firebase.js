387dd4a66bb2cf294d3e887db4243c62ef6070c4
hoon120735-hash <hoon120735@gmail.com>
2025-11-17 20:34:06 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,18 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // ✅ 이미지 저장용 추가!
+import { getAuth } from "firebase/auth";
 
-// Firebase 설정
 const firebaseConfig = {
-  apiKey: "AIzaSyBc1ieXZEOPpNiCEA1NPopQmR0h08UInw8",
-  authDomain: "nexuspick-88b83.firebaseapp.com",
-  projectId: "nexuspick-88b83",
-  storageBucket: "nexuspick-88b83.appspot.com",
-  messagingSenderId: "106940818503",
-  appId: "1:106940818503:web:890ee697bddf1c0fbf31a2",
-  measurementId: "G-YWJETLEW7V"
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
-// Firebase 초기화
 const app = initializeApp(firebaseConfig);
 
-// Firestore (데이터베이스)
 export const db = getFirestore(app);
-
-// Storage (이미지 저장용) ✅ 추가!
-export const storage = getStorage(app);
+export const auth = getAuth(app);
