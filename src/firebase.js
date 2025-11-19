edeea78612382769dcacebcd8b96c11447b67e41
hoon120735-hash <hoon120735@gmail.com>
2025-11-19 16:12:38 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBc1ieXZEOPpNiCEA1NPopQmR0h08UInw8",
@@ -10,12 +8,8 @@ const firebaseConfig = {
   storageBucket: "nexuspick-88b83.appspot.com",
   messagingSenderId: "106940810503",
   appId: "1:106940810503:web:890ee697bddf1c0fbf31a2",
-  measurementId: "G-YWJETLEW7V"
+  measurementId: "G-YWJETLEW7V",
 };
 
-// Firebase 초기화
 const app = initializeApp(firebaseConfig);
-
-// Firestore와 Auth 내보내기
 export const db = getFirestore(app);
-export const auth = getAuth(app);
