7d6d58a227a1b83ffbe3a9bf582826d2635e41bb
hoon120735-hash <hoon120735@gmail.com>
2025-11-19 16:53:00 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBc1ieXZEOPpNiCEA1NPopQmR0h08UInw8",
@@ -13,9 +12,5 @@ const firebaseConfig = {
   measurementId: "G-YWJETLEW7V"
 };
 
-// Firebase 초기화
 const app = initializeApp(firebaseConfig);
-
-// Firebase 인증 + Firestore 변수 생성
 export const auth = getAuth(app);
-export const db = getFirestore(app);
