35ebde5d4f09bf68d684bd367d641f1afde1d173
hoon120735-hash <hoon120735@gmail.com>
2025-11-19 16:37:19 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -8,8 +10,12 @@ const firebaseConfig = {
   storageBucket: "nexuspick-88b83.appspot.com",
   messagingSenderId: "106940810503",
   appId: "1:106940810503:web:890ee697bddf1c0fbf31a2",
-  measurementId: "G-YWJETLEW7V",
+  measurementId: "G-YWJETLEW7V"
 };
 
+// Firebase 초기화
 const app = initializeApp(firebaseConfig);
+
+// Firebase 인증 + Firestore 변수 생성
+export const auth = getAuth(app);
 export const db = getFirestore(app);
